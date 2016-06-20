971caf56ca138fe64d5187a39854be5035b988df
Trevor Keller <trevor.keller@nist.gov>
2016-06-20 11:04:02 -0400

    buggy scripts

--- src/qhold.sh
+++ src/qhold.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Usage: ./qsub.sh PREREQ_ID script_to_launch.sh
+# Usage: ./qsub.sh PREREQ_JOB_ID /full/path/to/script_to_launch.sh
 
 PRIOR=$1
 SCRIPT=$2
 CORES=64
+
+cd /users/tnk10/research/projects/trijunctionThreshold/dat
 
 qsub -cwd -m be -M tnk10 -pe nodal $CORES -q rack4,rack5 -hold_jid $PRIOR $SCRIPT
--- src/qsub.sh
+++ src/qsub.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Usage: ./qsub.sh script_to_launch.sh
+# Usage: ./qsub.sh /full/path/to/script_to_launch.sh
 
 SCRIPT=$1
 CORES=64
+
+cd /users/tnk10/research/projects/trijunctionThreshold/dat
 
 qsub -cwd -m be -M tnk10 -pe nodal $CORES -q rack4,rack5 $SCRIPT
